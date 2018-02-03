6ada3b8c259949a2b0826388544618a4e8e2fb4e
Infrastep <kunal@infrastep.com>
2018-02-03 09:56:05 +0530

    update

---
+++ BaseClass/VM/WithheldRefund/WITHHELD_REFUND_Base.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseClass.VM.WithheldRefund
+{
+   public class WITHHELD_REFUND_Base_WR
+   {
+       public int ID { get; set; }
+       public int EMP_CODE { get; set; }
+       public string REMARKS { get; set; }
+       public string AMOUNT { get; set; }
+       public string FINANCIAL_YEAR { get; set; }
+       public string REF_PREFIX { get; set; }
+       public Nullable<int> REF_NO { get; set; }
+       public Nullable<System.DateTime> REF_DATE { get; set; }
+
+       public virtual EMP_FIXED_Base_WR EMP_FIXED { get; set; }
+    }
+}
---
+++ BuisnessController/DUTY_TYPE_MASTERBC.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BaseClass;
+using DataAccess.Infrastructure;
+namespace BuisnessController
+{
+    public class DUTY_TYPE_MASTERBC
+    {
+       public static DUTY_TYPE_MASTER_Base insertupdateData(DUTY_TYPE_MASTER_Base obj)
+        {
+            DUTY_TYPE_MASTERDL ed = new DUTY_TYPE_MASTERDL();
+            return ed.insertdata(obj);
+        }
+
+       public static List<DUTY_TYPE_MASTER_Base> getdata()
+        {
+            DUTY_TYPE_MASTERDL ed = new DUTY_TYPE_MASTERDL();
+            return ed.getdata();
+        }
+       public static DUTY_TYPE_MASTER_Base getdata(int id)
+        {
+            DUTY_TYPE_MASTERDL ed = new DUTY_TYPE_MASTERDL();
+            return ed.getdata(id);
+        }
+    }
+}
